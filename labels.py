d0baee9d9b536c3f56ff705306e27b07bde9e3fe
Matt <5638426+mattbruv@users.noreply.github.com>
2020-09-24 17:58:11 -0400

    clean and optimize label code

--- labels.py
+++ labels.py
@@ -9,21 +9,6 @@ ignore_lbl_files = [
     "Runtime.s"
 ]
 
-def getLabels():
-    return list(filter(lambda x: x["type"] == "OBJECT" and x["scope"] == "GLOBAL", symbols))
-
-def getFuncs():
-    return list(filter(lambda x: x["type"] == "OBJECT" and x["scope"] != "GLOBAL", symbols))
-
-fnames = list(map(lambda x: x["name"], getFuncs()))
-
-"""
-for lbl in getLabels():
-    if lbl["name"] in fnames:
-        print(lbl["name"])
-"""
-
-
 def blacklistedAddrs():
     asm = getAsmFiles()
     addrs = []
@@ -39,40 +24,49 @@ def renameGlobalLabels():
     count = 0
     filecount = 0
     blacklist = blacklistedAddrs()
-    for f in asm:
-        if f.name in ignore_lbl_files:
+
+    for f in asm: # iterate assembly files
+        if f.name in ignore_lbl_files: # but not excluded ones
             continue
+
         filecount += 1
         data = open(f).read()
         matches = matchLabels(data)
         replacements = 0
-        for lbl in matches: 
-            name = getFunctionName(lbl)
-            if name == "LastState":
-                continue
+
+        for lbl in matches: # loop through every label in the file
+            name = getFunctionName(lbl) # name is None if not in symbol table
             if name is not None and lbl not in blacklist:
-                if not isValidName(name):
+                if not isValidName(name): # test for mangled c++ name
+                    # if  mangled, add a comment above the global definition
                     data = data.replace(".global lbl_" + lbl, "/* " + name + " */\n.global lbl_" + lbl)
                     print("sanitized", name)
                     name = sanitize(name)
+                # replace the label with name from symbol file
                 data = data.replace('lbl_' + lbl, name)
                 replacements += 1
                 names.add(name)
         count += replacements
         print(f, "replaced", replacements, "labels")
-        open(f, 'w').write(data)
+        open(f, 'w').write(data) # write new assembly file changes
     print("Replaced", count, "references of",  len(names), "unique global labels across", filecount, "files.")
 
+syms = list(filter(lambda x: x["type"] == "OBJECT" and x["scope"] == "GLOBAL", symbols))
+
+# hash map to speed up label lookups
+lookupDict = {}
+
+for s in syms:
+    addr = s["address"]
+    if addr not in lookupDict:
+        lookupDict[addr] = s
+    else:
+        # this should never be reachable
+        exit(1)
+
 def getFunctionName(address):
-    syms = list(filter(lambda x: x["type"] == "OBJECT" and x["scope"] == "GLOBAL" and x["address"] == address, symbols))
-    if len(syms) > 1:
-        print("ERROR", address, "HAS MULTIPLE REFS")
-        exit(69)
-    if len(syms) == 0:
+    if address not in lookupDict:
         return None
-    return syms[0]["name"]
+    return lookupDict[address]["name"]
 
 renameGlobalLabels()
-
-#print(blacklistedAddrs())
-#a = "render_fill_rect__FRC13basic_rect<f>10iColor_tag"
