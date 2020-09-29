e84ffeaefbdaf94aa0fe51077492bfb5af7e38dd
Matt <5638426+mattbruv@users.noreply.github.com>
2020-09-29 16:33:23 -0400

    fix script

--- inlineasm.py
+++ inlineasm.py
@@ -7,8 +7,6 @@ desc = "Automatically move and inline assembly into CPP files"
 parser = argparse.ArgumentParser(description=desc)
 parser.add_argument("asmFile", help="The assembly file to process")
 parser.add_argument("-l", "--link", help="Reference the new src file in obj_files.mk", action="store_true")
-#parser.add_argument("-d", '--defineTop', help="Define all symbols at one spot at top of the C++ file, as apposed to above each function", action="store_true")
-#parser.add_argument("--toggle", help="Include a #DEFINE to toggle between assembly and cpp")
 
 def getSource(name):
     files = list(filter(lambda x: name.lower() in x.name.lower(), getAsmFiles()))
@@ -23,55 +21,26 @@ def getSource(name):
         quit()
     return files[0]
 
+def format(c, f, path):
+    c += "\n"
+    c += "#pragma GLOBAL_ASM(\"" + path + "\", \""
+    c += f + "\")\n"
+    return c
+
 def run():
     args = parser.parse_args()
     sourceFile = getSource(args.asmFile)
     asm = open(sourceFile, "r").read()
-    #asm = desanitize(asm)
+
+    out = str(sourceFile).replace("asm\\", "src\\").replace(".s", ".cpp")
+
+    name = sourceFile.name.replace(".s", ".h")
+    code = '#include "' + name + '"\n'
     funcs = getAsmFunctions(asm)
-
-    # collect all global labels
-    labels = set()
-    funcLabels = set()
-    exclude = set(getLabels(asm, strip=True)) # function labels from this file
-
     for f in funcs:
-        block = getAsmFunctionBlock(asm, f)
-        lbls = getBlockLabels(block)
-        fs = getBlockFunctionCalls(block)
-        funcLabels.update(fs)
-        labels.update(lbls)
-    
-    funcLabels = funcLabels - exclude
-    externLabels = labels - funcLabels
-    #print(externLabels)
-    #print(sourceFile)
-    cppsource = '#include "' + sourceFile.name.replace('.s', '.h') + '"' + "\n\n"
-    extern = []
-
-    extern.append("// Functions in " + sourceFile.name.replace('.s', '.cpp'))
-    fileFuncs = getAsmFunctions(asm, strip=True)
-    extern += list(map(funcString, fileFuncs))
-
-    extern += ["", "// external function references"]
-    fs = sorted(list(funcLabels))
-    fs = list(map(funcString, fs))
-    extern += fs
-
-    extern += ["", "// assembly references"]
-    lbls = sorted(list(externLabels))
-    lbls = list(map(labelString, lbls))
-    extern += lbls
-
-    cppsource += externBlock(extern) + "\n"
-
-    for f in funcs:
-        block = getAsmFunctionBlock(asm, f)
-        code = blockToCPP(f, block)
-        cppsource += code + "\n"
-
-
-    outPath = asmToSrcPath(sourceFile.__str__())
-    open(outPath, "w", newline="").write(cppsource)
+        p = str(sourceFile)[14::].replace('\\', '/')
+        code = format(code, f.replace(':', ''), p)
+        pass
+    open(out, "w").write(code)
 
 run()
